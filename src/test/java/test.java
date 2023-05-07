d74ddfc3a561b69dca4f022e9e657458d87d61d1
xiaoman715 <xiaomanyue1020@163.com>
2023-05-07 16:02:58 +0800

    Initial commit

---
+++ src/test/java/test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class test {
+
+    @Test
+    public  void ss(){
+        System.out.println("111");
+        
+    }
+}
